9b540ace2eef6df4d6de72902311dc07aba05151
plrtp <plrtp@mail.ru>
2024-12-24 15:27:27 +0300

    сущности корабль (Ship) добавлена высота и ширина

--- src/entities/Ship.ts
+++ src/entities/Ship.ts
@@ -1,13 +1,15 @@
-import { LinearMovement } from '../actions/LinearMovement.js';
 import { Coordinates } from './Coordinates.js';
 
-export class Ship extends LinearMovement{
+export class Ship {
   coordinates: Coordinates;
+  width: number;
+  height: number;
   isDragging: boolean;
 
-  constructor(coordinates: Coordinates, isDragging: boolean = false) {
-    super(coordinates)
+  constructor(coordinates: Coordinates, width: number, height: number, isDragging: boolean = false,) {
     this.coordinates = coordinates;
+    this.width = width;
+    this.height = height;
     this.isDragging = isDragging;
   }
 }
