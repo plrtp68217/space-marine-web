36efe6b2cafa4707555b8a07a78c2c4600a7cbdb
plrtp <plrtp@mail.ru>
2024-12-24 11:00:14 +0300

    для сущности -  Корабль добавлено свойство: isDragging для перемещения способом drag and drop

--- src/entities/Ship.ts
+++ src/entities/Ship.ts
@@ -3,9 +3,11 @@ import { Coordinates } from './Coordinates.js';
 
 export class Ship extends LinearMovement{
   coordinates: Coordinates;
+  isDragging: boolean;
 
-  constructor(coordinates: Coordinates) {
+  constructor(coordinates: Coordinates, isDragging: boolean = false) {
     super(coordinates)
     this.coordinates = coordinates;
+    this.isDragging = isDragging;
   }
 }
