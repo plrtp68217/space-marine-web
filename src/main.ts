ba0aabcfc24d3416c0d9335a7a739bbbbc0249e6
plrtp <plrtp@mail.ru>
2024-12-24 11:01:02 +0300

    изменения main.ts на основе нововведений

--- src/main.ts
+++ src/main.ts
@@ -2,28 +2,37 @@ import "./options/resizeCanvas.js";
 
 import { Ship } from "./entities/Ship.js";
 import { Coordinates } from "./entities/Coordinates.js";
+
 import { drawBullets } from "./draw-functions/drawBullets.js";
 import { drawEnemys } from "./draw-functions/drawEnemys.js"; 
 import { drawShip } from "./draw-functions/drawShip.js";
+import { drawClear } from "./draw-functions/drawClear.js";
+
+import { mouseDragShip, mouseDropShip, mouseMoveShip } from "./ship/mouse/moveShip.js";
+import { touchDragShip, touchDropShip, touchMoveShip } from "./ship/touch/moveShip.js";
 
 let canvas = document.querySelector('#canvas') as HTMLCanvasElement;
 let ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 
-const ship_coordinates = new Coordinates(100, 200);
+const shipStartX = canvas.width / 2;
+const shipStartY = (canvas.height * 3) / 4;
+
+const ship_coordinates = new Coordinates(shipStartX, shipStartY);
 const ship = new Ship(ship_coordinates);
 
-const button_right = document.querySelector('.right') as HTMLButtonElement;
-const button_left = document.querySelector('.left') as HTMLButtonElement;
+canvas.addEventListener('mousedown', () => mouseDragShip(ship));
+canvas.addEventListener('mousemove', (event: MouseEvent) => mouseMoveShip(event, ship));
+canvas.addEventListener('mouseup', () => mouseDropShip(ship));
 
-button_right.addEventListener('mousedown', () => ship.moveRight());
-button_left.addEventListener('mousedown', () => ship.moveLeft());
+canvas.addEventListener('touchstart', () => touchDragShip(ship));
+canvas.addEventListener('touchmove', (event: TouchEvent) => touchMoveShip(event, ship));
+canvas.addEventListener('touchend', () => touchDropShip(ship));
 
 
 function render(): void {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawClear(canvas, ctx);
   drawShip(ship, ctx);
   requestAnimationFrame(render)
 }
 
-
-render()
+render();
