866e94eaab9892398dc3b67649fb4e87fcd82401
plrtp <plrtp@mail.ru>
2024-12-24 10:58:50 +0300

    добавлен стиль заливки у функций, отвечающих за отрисовку корябля, пуль и противников

--- src/draw-functions/drawBullets.ts
+++ src/draw-functions/drawBullets.ts
@@ -2,6 +2,7 @@ import { Bullet } from "../entities/Bullet.js";
 
 export function drawBullets(bullets: Bullet[], context: CanvasRenderingContext2D): void {
     for (let bullet of bullets) {
+        context.fillStyle = 'white';
         context.fillRect(bullet.coordinates.x, bullet.coordinates.y, 5, 5);
     }
 }
--- src/draw-functions/drawShip.ts
+++ src/draw-functions/drawShip.ts
@@ -1,5 +1,6 @@
 import { Ship } from "../entities/Ship.js";
 
 export function drawShip(ship: Ship, context: CanvasRenderingContext2D): void {
+    context.fillStyle = 'white';
     context.fillRect(ship.coordinates.x, ship.coordinates.y, 10, 10);
 }
