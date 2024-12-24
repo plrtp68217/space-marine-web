6b1c06f6cbdf6c5942d6d2ce46054fa9bf4e3e97
plrtp <plrtp@mail.ru>
2024-12-24 15:25:07 +0300

    движение кораблем в директории ship/movement

---
+++ src/ship/movement/mouse/moveShip.ts
@@ -0,0 +1,17 @@
+import { Ship } from "../../../entities/Ship.js";
+
+
+export function mouseDragShip(ship: Ship) {
+    ship.isDragging = true;
+}
+
+export function mouseMoveShip(event: MouseEvent, ship: Ship) {
+    if (ship.isDragging) {
+        ship.coordinates.x = event.clientX;
+        ship.coordinates.y = event.clientY;
+    }
+}
+
+export function mouseDropShip(ship: Ship) {
+    ship.isDragging = false;
+}
